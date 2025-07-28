bc2df7a6993333e6e660d08188c65db7d68d7118
BDicksmusic <Bdicksmusic@gmail.com>
2025-07-27 23:59:25 -0700

    Update notion-pdf-handler.js

--- src/notion-pdf-handler.js
+++ src/notion-pdf-handler.js
@@ -80,7 +80,7 @@ class NotionPDFHandler {
     }
 
     // Add Status property
-    properties['Status'] = await this.buildStatusProperty('📄');
+    properties['Status'] = await this.buildStatusProperty('📥');
 
     // Add document type as relation
     properties['Document Type'] = await this.buildDocumentTypeRelation(metadata?.fileType);
